34b38dbe7544bf36c69c3f764729fece37f99252
Roland Rodriguez <roland@govcraft.ai>
2024-06-09 14:25:59 -0600

    chore: remove unnecessary import in tangler_config.rs Removed an unused import statement in tangler_config.rs to clean up the code.

--- src/tangler_config.rs
+++ src/tangler_config.rs
@@ -2,7 +2,6 @@ use serde::Deserialize;
 
 use crate::repository_config::RepositoryConfig;
 
-// TODO: Revisit and make sure we need this
 #[derive(Deserialize, Debug)]
 pub(crate) struct TanglerConfig {
     pub(crate) repositories: Vec<RepositoryConfig>,
