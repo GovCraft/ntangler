91bd9415f715a6080b64370abf94dd5cf3322223
Roland Rodriguez <roland@govcraft.ai>
2024-06-09 14:59:09 -0600

    chore: remove duplicate use statement for OpenAi Removed the duplicate use statement for OpenAi in src/actors.rs.

--- src/actors.rs
+++ src/actors.rs
@@ -1,10 +1,10 @@
-pub(crate) use generators::OpenAi;
 pub(crate) use brokers::Broker;
+pub(crate) use generators::OpenAi;
 pub(crate) use sentinels::GitSentinel;
 pub(crate) use tangler_actor::TanglerActor;
 
 mod tangler_actor;
-mod repository;
+mod repositories;
 mod sentinels;
 mod generators;
 mod brokers;
