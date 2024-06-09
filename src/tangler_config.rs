34fdfc04355ece0b3716763a42c029ee85068647
Roland Rodriguez <roland@govcraft.ai>
2024-06-09 13:12:14 -0600

    chore: remove duplicate import of RepositoryConfig Removed the duplicate import of RepositoryConfig from tangler_config.rs.

--- src/tangler_config.rs
+++ src/tangler_config.rs
@@ -1,6 +1,8 @@
-use crate::repository_config::RepositoryConfig;
 use serde::Deserialize;
 
+use crate::repository_config::RepositoryConfig;
+
+// TODO: Revisit and make sure we need this
 #[derive(Deserialize, Debug)]
 pub(crate) struct TanglerConfig {
     pub(crate) repositories: Vec<RepositoryConfig>,
