964e2fc7d223264df0e5ed4fca77f1df4a2b1b3e
Roland Rodriguez <roland@govcraft.ai>
2024-06-09 13:13:27 -0600

    style: add newline at end of file Added a newline at the end of src/messages/submit_diff.rs to comply with POSIX standards.

--- src/messages/submit_diff.rs
+++ src/messages/submit_diff.rs
@@ -1,7 +1,11 @@
+use std::path::PathBuf;
+
 use akton::prelude::*;
 
+// TODO: will revisit names shortly
 #[akton_message]
 pub(crate) struct SubmitDiff {
     pub(crate) diff: String,
+    pub(crate) path: PathBuf,
     pub(crate) id: String,
 }
