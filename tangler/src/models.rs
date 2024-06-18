95cd630c6ded3ec23f917c832da08269e2f03c4f
Roland Rodriguez <roland@govcraft.ai>
2024-06-17 22:13:32 -0600

    feat(models): add generate_id function
    Introduce generate_id function to create a unique ID based on the hash of the repository and filename combined. This function uses DefaultHasher to generate the hash and combines the repository and filename hashes to produce a unique identifier.
    
    NEW FEATURE: You appear to have introduced one or more new features that are backward-compatible. According to Semantic Versioning (SemVer), this requires a minor version update. Please verify and update your version number accordingly.

--- tangler/src/models.rs
+++ tangler/src/models.rs
@@ -1,3 +1,5 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+pub(crate) use pending_commit::PendingCommit;
 pub(crate) use commit::Commit;
 pub(crate) use commit_type::CommitType;
 pub(crate) use description::Description;
@@ -6,7 +8,7 @@ pub(crate) use oid::Oid;
 pub(crate) use scope::{OptionalScope, Scope};
 pub(crate) use time_stamp::TimeStamp;
 pub(crate) use ui::*;
-
+pub(crate) use file_name::Filename;
 mod footer;
 mod commit_type;
 mod scope;
@@ -18,4 +20,15 @@ mod description;
 mod semver_impact;
 mod ui;
 mod traits;
+mod file_name;
+mod pending_commit;
+
+/// Generates a unique ID based on the hash of the repository and filename combined.
+pub(crate) fn generate_id(repository: &str, filename: Filename) -> String {
+    let mut hasher = DefaultHasher::new();
+    repository.hash(&mut hasher);
+    filename.hash(&mut hasher);
+    hasher.finish().to_string()
+}
+
 
