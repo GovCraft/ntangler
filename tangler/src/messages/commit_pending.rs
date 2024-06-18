ae56bfe6d58470153b6cc8db405e8d697276e4ff
Roland Rodriguez <roland@govcraft.ai>
2024-06-17 22:13:33 -0600

    feat(messages): add CommitPending struct
    Introduce the CommitPending struct to represent a successful commit message with its details. The struct includes a PendingCommit field and provides methods to access and create new instances of CommitPending.
    
    NEW FEATURE: You appear to have introduced one or more new features that are backward-compatible. According to Semantic Versioning (SemVer), this requires a minor version update. Please verify and update your version number accordingly.

---
+++ tangler/src/messages/commit_pending.rs
@@ -0,0 +1,19 @@
+use akton::prelude::*;
+
+use crate::models::{Commit, Oid, PendingCommit, TimeStamp};
+
+/// Represents a successful commit message with its details.
+#[akton_message]
+pub(crate) struct CommitPending {
+    commit: PendingCommit
+}
+
+impl CommitPending {
+    pub(crate) fn commit(&self) -> &PendingCommit {
+        &self.commit
+    }
+    pub(crate) fn new(mut commit: PendingCommit, hash: String) -> CommitPending {
+        CommitPending { commit }
+    }
+}
+
