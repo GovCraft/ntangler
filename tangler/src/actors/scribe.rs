20146ae5cc60639a11fc863e405b7862c5b54367
Roland Rodriguez <roland@govcraft.ai>
2024-06-17 22:13:36 -0600

    refactor(actors): update event handling
    Refactor the Scribe actor to improve event handling.
    
    Changes include:
    - Add handling for CommitEvent to update the event queue.
    - Sort events by timestamp and display the latest 10 events.
    - Clear the console before printing events.
    - Update the SystemStarted handler to clear the screen and display instructions.
    - Remove redundant code and merge similar functionality.

--- tangler/src/actors/scribe.rs
+++ tangler/src/actors/scribe.rs
@@ -1,14 +1,17 @@
 use std::{any::TypeId, fmt, fmt::Display};
-use std::fmt::Debug;
+use std::collections::VecDeque;
+use std::fmt::{Debug, format};
 use std::io::Write;
 use std::ops::Deref;
 use std::str::FromStr;
+use std::time::Duration;
 
 use akton::prelude::*;
 use akton::prelude::async_trait::async_trait;
 use atty::is;
 use chrono::{DateTime, Local, NaiveDateTime, TimeZone, Utc};
 use console::{style, StyledObject, Term, Color as TermColor};
+use indicatif::{ProgressBar, ProgressStyle};
 use owo_colors::OwoColorize;
 use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 use tokio::sync::mpsc;
@@ -16,10 +19,11 @@ use tracing::{error, trace};
 use unicode_segmentation::UnicodeSegmentation;
 use unicode_width::UnicodeWidthStr;
 
-use crate::messages::{CommitSuccess, NotifyError, SubscribeBroker, SystemStarted};
-use crate::models::{TEAL_11, TEAL_9, TEAL_12, CommitTypeTerminal, DescriptionTerminal, DimStatic, OidTerminal, OptionalScope, Scope, ScopeTerminal, SemVerImpactTerminal, GRASS_11, GRASS_12, TimeStampTerminal, RED_9, GRAY_10, TEAL_7, RED_11, GRAY_9, GRAY_7, GRAY_12, WHITE_PURE, GRAY_11, IsBreakingTerminal, CommitHeadingTerminal, CommitType, RepositoryTerminal, ACCENT, INSTRUCTIONS, BRAND_NAME, HR_COLOR};
+use crate::messages::{Category, CommitEvent, CommitPending, CommitPosted, NotifyError, SubscribeBroker, SystemStarted};
+use crate::models::{TEAL_11, TEAL_9, TEAL_12, CommitTypeTerminal, DescriptionTerminal, DimStatic, OidTerminal, OptionalScope, Scope, ScopeTerminal, SemVerImpactTerminal, GRASS_11, GRASS_12, TimeStampTerminal, RED_9, GRAY_10, TEAL_7, RED_11, GRAY_9, GRAY_7, GRAY_12, WHITE_PURE, GRAY_11, IsBreakingTerminal, CommitHeadingTerminal, CommitType, RepositoryTerminal, ACCENT, INSTRUCTIONS, BRAND_NAME, HR_COLOR, FilenameTerminal, PendingCommit, Commit};
 
 const TAB_WIDTH: usize = 8; // You can set this to any number of spaces you want
+const LIST_ROW: usize = 3;
 
 #[akton_actor]
 pub(crate) struct Scribe {
@@ -28,6 +32,7 @@ pub(crate) struct Scribe {
     stderr: Option<Term>,
     tab: String,
     half_tab: String,
+    events: VecDeque<CommitEvent>,
 }
 
 impl Scribe {
@@ -56,50 +61,71 @@ impl Scribe {
                     stderr.write_line(&format!("{}", error_message));
                 }
             })
-            .act_on::<CommitSuccess>(|actor, event| {
+            .act_on::<CommitEvent>(|actor, event| {
+                let event_id = &event.message.id;
 
-                let tab = &actor.state.tab;
-                //format user message
-                let half_tab = &actor.state.half_tab;
+                // Check if an event with the same ID already exists in the queue
+                let mut replaced = false;
+                for existing_event in actor.state.events.iter_mut() {
+                    let existing_id = &existing_event.id;
+                    if existing_id == event_id {
+                        *existing_event = event.message.clone();
+                        replaced = true;
+                        break;
+                    }
+                }
 
-                let commit = event.message.commit();
-                let repository = &commit.repository;
-                let oid = &commit.oid;
-                let description = &commit.description;
-                let scope = &commit.scope;
-                let time_stamp = &commit.time_stamp;
-                let commit_type = &commit.commit_type;
-                let semver_impact = &commit.semver_impact;
-                let is_breaking = &commit.is_breaking;
+                // If no existing event was replaced, push the new event to the front of the deque
+                if !replaced {
+                    actor.state.events.push_front(event.message.clone());
+                }
 
-                // convert to terminal formatted versions
-                let oid: OidTerminal = oid.into();
-                let description: DescriptionTerminal = description.into();
-                let time_stamp: TimeStampTerminal = time_stamp.into();
-                let semver_impact: SemVerImpactTerminal = semver_impact.into();
-                let commit_heading: (CommitTypeTerminal, ScopeTerminal, IsBreakingTerminal) = (commit_type.into(), scope.into(), is_breaking.into());
-                let commit_heading: CommitHeadingTerminal = commit_heading.into();
-                let repository: RepositoryTerminal = repository.into();
+                // If the deque exceeds 10 events, pop the back (oldest) event
+                if actor.state.events.len() > 10 {
+                    actor.state.events.pop_back();
+                }
 
-                eprintln!("{half_tab}{repository} {time_stamp} {oid} {semver_impact} {commit_heading} {description}");
+                // Clear the console before printing
+                if let Some(stderr) = &actor.state.stderr {
+                    // Move the cursor to the top-left corner
+                    stderr.move_cursor_to(0, LIST_ROW).unwrap();
+
+                    // Clear the screen
+                    stderr.clear_to_end_of_screen();
+                    // Convert VecDeque to Vec
+                    let mut events: Vec<_> = actor.state.events.clone().drain(..).collect();
+
+                    // Sort the Vec by timestamp from newest to oldest
+                    events.sort_by(|a, b| b.timestamp.cmp(&a.timestamp));
+
+                    // Print only the last 10 events
+                    for event in &events {
+                        stderr.clear_line();
+                       stderr.write_line(event.to_string().as_str()).unwrap();
+                    }
+                }
             })
             .act_on::<SystemStarted>(|actor, event| {
                 // Determine the appropriate ColorChoice for stdout and stderr
+                if let Some(stderr) = &actor.state.stderr {
+                    stderr.clear_screen();
 
-                // Write colored text to stderr using termcolor
-                let tangler = "Tangler".style(BRAND_NAME.clone());
-                let instructions = " Ctrl-C (Stop)".style(INSTRUCTIONS.clone().clone());
-                eprintln!( "{}{tangler}{instructions}", actor.state.half_tab);
-                actor.state.hr();
+                    stderr.move_cursor_to(0, LIST_ROW - 2).unwrap();
+                    // Write colored text to stderr using termcolor
+                    let tangler = "Tangler".style(BRAND_NAME.clone());
+                    let instructions = "Ctrl-C (Stop)".style(INSTRUCTIONS.clone().clone());
+                    let display = format!("{}{tangler} {instructions}", actor.state.half_tab);
+                    stderr.write_line(&display);
+                    actor.state.hr();
+                }
             });
 
         let subscription = SubscribeBroker {
             subscriber_id: actor.key.value.clone(),
-            message_type_id: TypeId::of::<CommitSuccess>(),
+            message_type_id: TypeId::of::<CommitEvent>(),
             subscriber_context: actor.context.clone(),
         };
-        trace!(type_id=?TypeId::of::<CommitSuccess>(),subscriber=actor.key.value.clone(),"Subscribed to CommitSuccess:");
-
+        trace!(type_id=?TypeId::of::<CommitEvent>(),subscriber=actor.key.value.clone(),"Subscribed to CommitEvent:");
         actor.state.broker.emit_async(subscription, None).await;
 
         let subscription = SubscribeBroker {
@@ -125,54 +151,4 @@ impl Scribe {
             eprintln!("{}{}", &self.half_tab, hr.style(HR_COLOR.clone()));
         }
     }
-    fn style_time(datetime: &str) -> StyledObject<String> {
-        let parts: Vec<&str> = datetime.splitn(3, ' ').collect();
-        if parts.len() == 3 {
-            let prefix = parts[0];
-            let date = parts[1];
-            let time = parts[2];
-
-            // Parse the date and time
-            let datetime_str = format!("{} {}", date, time);
-            let naive_datetime =
-                NaiveDateTime::parse_from_str(&datetime_str, "%Y-%m-%d %H:%M:%S").unwrap();
-            let local_datetime = Local.from_local_datetime(&naive_datetime).unwrap();
-            let now = Local::now();
-
-            // Calculate the relative time
-            let duration = now.signed_duration_since(local_datetime);
-
-            // Style the time and date
-            let styled_time = style(time).color256(250).to_string();
-            let styled_date = style(date).color256(250).to_string();
-            //let styled_relative_time = style(relative_time).color256(204).to_string();
-
-            // Format the final output
-            let styled_datetime = format!("{} {}", styled_date, styled_time);
-            style(styled_datetime)
-        } else {
-            style(datetime.to_string())
-        }
-    }
-    fn style_commit_msg(msg: &str) -> StyledObject<String> {
-        if let Some((prefix, rest)) = msg.split_once(':') {
-            let styled_prefix = style(prefix).color256(31).to_string();
-            let styled_msg = format!("{}:{}", styled_prefix, style(rest).color256(253));
-            style(styled_msg)
-        } else {
-            style(msg.to_string())
-        }
-    }
-
-    fn print_indented_message(msg: impl AsRef<str> + Display) {
-        let msg_str = msg.as_ref();
-        let formatted_msg = format!("{:>width$}", msg_str, width = msg_str.len() + 0);
-        eprintln!("{}", formatted_msg);
-    }
-    fn print_commit_message(msg: impl AsRef<str> + Display) {
-        let msg = format!("[{}] {}", style("Tangler").color256(253), msg);
-        eprintln!("{}", msg);
-    }
-
-
 }
