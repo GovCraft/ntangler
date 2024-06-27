6b6de0e7f958e4b73377e21d5bf322680a972489
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 09:56:32 -0600

    refactor(messages): remove unused imports
    This commit removes unused imports from the messages module. The following imports have been removed:
    - poll_changes
    - system_started
    This cleanup helps to maintain a cleaner and more efficient codebase.

--- tangler/src/messages.rs
+++ tangler/src/messages.rs
@@ -1,6 +1,3 @@
-
-
-
 mod poll_changes;
 mod system_started;
 
@@ -11,14 +8,13 @@ mod diff_queued;
 mod file_change_detected;
 mod finalized_commit;
 mod generation_started;
+pub(crate) use commit_message_generated::CommitMessageGenerated;
 pub(crate) use finalized_commit::FinalizedCommit;
 pub(crate) use generation_started::GenerationStarted;
-pub(crate) use commit_message_generated::CommitMessageGenerated;
 
 pub(crate) use poll_changes::RepositoryPollRequested;
 
 pub(crate) use system_started::SystemStarted;
-
 
 // pub(crate) use commit_authoring::CommitAuthoring;
 pub(crate) use diff_queued::DiffQueued;
