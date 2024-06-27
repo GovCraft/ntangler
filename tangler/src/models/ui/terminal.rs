ddd73e44792009678327b0542668776f4900d85f
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 11:24:07 -0600

    chore(ui): remove unused imports
    Remove commented out and unused imports from terminal.rs. This cleanup improves code readability and maintainability by eliminating dead code.

--- tangler/src/models/ui/terminal.rs
+++ tangler/src/models/ui/terminal.rs
@@ -2,15 +2,11 @@ pub(crate) use app_event::AppEvent;
 pub(crate) use commit_heading::CommitHeadingTerminal;
 pub(crate) use commit_type::CommitTypeTerminal;
 pub(crate) use description::DescriptionTerminal;
-
-pub(crate) use filename::FilenameTerminal;
 pub(crate) use is_breaking::IsBreakingTerminal;
 pub(crate) use oid::OidTerminal;
-pub(crate) use repository::RepositoryTerminal;
 pub(crate) use scope::ScopeTerminal;
 pub(crate) use semver_impact::SemVerImpactTerminal;
 
-// pub(crate) use commit_message_generated_commit::CommitMessageGeneratedCommit;
 mod commit_type;
 mod description;
 pub mod oid;
@@ -23,4 +19,3 @@ mod commit_heading;
 mod filename;
 mod is_breaking;
 mod repository;
-// pub mod commit_message_generated_commit;
