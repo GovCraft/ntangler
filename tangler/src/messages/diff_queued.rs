34c402570fc08cd94e9a91abbff2c40c510db400
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 02:27:38 -0600

    style(messages): remove duplicate imports
    Remove duplicate imports of CommittedCommit, Oid, and TimeStamp from the diff_queued.rs file. Clean up struct attributes by removing an extra comma in the derive macro.

--- tangler/src/messages/diff_queued.rs
+++ tangler/src/messages/diff_queued.rs
@@ -1,17 +1,14 @@
-use std::path::PathBuf;
+use crate::models::{CommittedCommit, Oid, TimeStamp};
 use akton::prelude::*;
 use derive_more::*;
 use derive_new::new;
-use crate::models::{CommittedCommit, Oid, TimeStamp};
+use std::path::PathBuf;
 
 /// Represents a successful commit message with its details.
-#[derive(new, Default, Debug, Clone, )]
+#[derive(new, Default, Debug, Clone)]
 pub(crate) struct DiffQueued {
     pub(crate) diff: String,
     pub(crate) target_file: PathBuf,
     pub(crate) repository_nickname: String,
     pub(crate) reply_address: Context,
 }
-
-
-
