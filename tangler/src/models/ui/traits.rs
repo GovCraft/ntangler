b2651c1dc625e2de4cc3012e08dfcd85cdf41721
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 02:26:29 -0600

    style(ui): fix formatting issues
    Corrects formatting issues in traits.rs by adjusting indentation and ensuring consistent style. This change does not alter the functionality of the code.

--- tangler/src/models/ui/traits.rs
+++ tangler/src/models/ui/traits.rs
@@ -1,11 +1,11 @@
-use std::fmt::Display;
+use crate::models::signature::TangledSignature;
+use crate::models::{Oid, TangledCommit, TangledRepository};
 use akton::prelude::Arn;
 use git2::Signature;
+use std::fmt::Display;
 use termcolor::ColorChoice;
-use crate::models::{ Oid, TangledCommit, TangledRepository};
-use crate::models::signature::TangledSignature;
 
-pub(crate) trait ConsoleStyle: Display{
+pub(crate) trait ConsoleStyle: Display {
     fn determine_color_choice(&self, stream: atty::Stream) -> ColorChoice {
         if atty::is(stream) {
             if self.supports_truecolor() {
@@ -20,11 +20,13 @@ pub(crate) trait ConsoleStyle: Display{
     // Function to check if the terminal supports true color
     fn supports_truecolor(&self) -> bool {
         // This is a simple heuristic. Likely need a more robust check.
-        std::env::var("COLORTERM").map_or(false, |colorterm| colorterm == "truecolor" || colorterm == "24bit")
+        std::env::var("COLORTERM").map_or(false, |colorterm| {
+            colorterm == "truecolor" || colorterm == "24bit"
+        })
     }
 }
 
-pub(crate) trait RepositoryEvent: Send + Sync + 'static{
+pub(crate) trait RepositoryEvent: Send + Sync + 'static {
     fn get_repo_info(&self) -> TangledRepository;
     // fn get_commit_step(&self) -> CommitStep;
     fn get_commit(&self) -> &TangledCommit;
@@ -38,5 +40,4 @@ pub(crate) trait TanglerCommit {
 
     fn get_summary(&self) -> Option<&str>;
     fn get_body(&self) -> Option<&str>;
-
 }
