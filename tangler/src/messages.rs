9e038545f8ccac57bcdf4082c2b1655fe4531ccb
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 09:50:28 -0600

    chore(messages): remove unused imports
    Comment out unused imports in messages.rs. The following imports are commented out:
    - git_repository_event
    - commit_event
    - commit_event::CommitEventCategory
    This cleanup helps in maintaining a cleaner and more efficient codebase.

--- tangler/src/messages.rs
+++ tangler/src/messages.rs
@@ -1,13 +1,10 @@
 
 
 
-mod accept_broker;
-mod commit_posted;
 mod poll_changes;
 mod system_started;
 
 mod commit_event;
-// mod git_repository_event;
 mod commit_finalized;
 mod commit_message_generated;
 mod diff_queued;
@@ -16,10 +13,6 @@ mod finalized_commit;
 mod generation_started;
 pub(crate) use finalized_commit::FinalizedCommit;
 pub(crate) use generation_started::GenerationStarted;
-// pub(crate) use git_repository_event::GitRepositoryEvent;
-// pub(crate) use commit_event::CommitEvent;
-// pub(crate) use commit_event::CommitEventCategory;
-pub(crate) use accept_broker::AcceptBroker;
 pub(crate) use commit_message_generated::CommitMessageGenerated;
 
 pub(crate) use poll_changes::RepositoryPollRequested;
