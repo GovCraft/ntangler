c7dcdd7d39135777cfda2b2ee6f1e20911542cea
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 02:27:32 -0600

    style(messages): remove duplicate imports
    This commit removes duplicate imports in the commit_message_generated.rs file. The imports of PathBuf and models were repeated. This cleanup helps to maintain cleaner and more readable code.

--- tangler/src/messages/commit_message_generated.rs
+++ tangler/src/messages/commit_message_generated.rs
@@ -1,15 +1,12 @@
-use std::path::PathBuf;
+use crate::models::{CommitMessage, CommittedCommit, Oid, TimeStamp};
 use akton::prelude::*;
 use derive_more::*;
 use derive_new::new;
-use crate::models::{CommitMessage, CommittedCommit, Oid, TimeStamp};
+use std::path::PathBuf;
 
 /// Represents a successful commit message with its details.
-#[derive(new, Default, Debug, Clone, )]
+#[derive(new, Default, Debug, Clone)]
 pub(crate) struct CommitMessageGenerated {
     pub(crate) target_file: PathBuf,
     pub(crate) commit_message: CommitMessage,
 }
-
-
-
