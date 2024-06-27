aa00cdb193d55ed5eb812a7e624c09e90f6d6f78
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 00:46:38 -0600

    feat(ui): add TAB_WIDTH constant
    Introduce TAB_WIDTH constant to manage tab width in UI strings. Add TAB and HALFTAB static references using TAB_WIDTH for consistent spacing. Update import statements accordingly.
    
    NEW FEATURE: You appear to have introduced one or more new features that are backward-compatible. According to Semantic Versioning (SemVer), this requires a minor version update. Please verify and update your version number accordingly.

--- tangler/src/models/ui/ui_strings.rs
+++ tangler/src/models/ui/ui_strings.rs
@@ -1,4 +1,4 @@
-use crate::models::{REPO_COLOR, TIME_COLOR};
+use crate::models::{REPO_COLOR, TAB_WIDTH, TIME_COLOR};
 use lazy_static::lazy_static;
 use owo_colors::OwoColorize;
 
@@ -9,8 +9,10 @@ pub(crate) const COLUMN_HEADING_FOUR: &str = "SEMVERS";
 pub(crate) const COLUMN_HEADING_FIVE: &str = "FILENAME";
 pub(crate) const COLUMN_HEADING_SIX: &str = "TYPE(SCOPE)";
 pub(crate) const COLUMN_HEADING_SEVEN: &str = "SUMMARY";
-
+pub(crate) const EMDASH: &str = "\u{2022}";
 lazy_static! {
+    pub static ref TAB: String = " ".repeat(TAB_WIDTH);
+    pub static ref HALFTAB: String = " ".repeat(TAB_WIDTH/2);
     pub static ref COLUMN_HEADING_ONE_LENGTH: usize = COLUMN_HEADING_ONE.len();
     pub static ref COLUMN_HEADING_TWO_LENGTH: usize = COLUMN_HEADING_TWO.len() + 4;
     pub static ref COLUMN_HEADING_THREE_LENGTH: usize = COLUMN_HEADING_THREE.len() + 5;
