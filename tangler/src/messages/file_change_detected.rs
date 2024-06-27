e95f174bb24c3cdc6ced2abbc09ab926353b4b41
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 02:26:38 -0600

    style(messages): remove duplicate imports
    Remove duplicate imports in file_change_detected.rs. This includes removing redundant imports of PathBuf and models (CommittedCommit, Oid, TimeStamp).

--- tangler/src/messages/file_change_detected.rs
+++ tangler/src/messages/file_change_detected.rs
@@ -1,14 +1,11 @@
-use std::path::PathBuf;
+use crate::models::{CommittedCommit, Oid, TimeStamp};
 use akton::prelude::*;
 use derive_more::*;
 use derive_new::new;
-use crate::models::{CommittedCommit, Oid, TimeStamp};
+use std::path::PathBuf;
 
 /// Represents a successful commit message with its details.
-#[derive(new, Default, Debug, Clone, )]
+#[derive(new, Default, Debug, Clone)]
 pub(crate) struct FileChangeDetected {
     pub(crate) path: PathBuf,
 }
-
-
-
