e47b4c70bfc1bbfeb6325197d2b64361a6f8fd69
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 02:15:37 -0600

    feat(messages): add FinalizedCommit struct
    Introduce the FinalizedCommit struct to represent a successful commit message with its details. This struct includes fields for when the commit occurred, the target file path, the repository nickname, the commit hash, and the commit message. Derive traits for new, Default, Debug, and Clone for ease of use.
    
    NEW FEATURE: You appear to have introduced one or more new features that are backward-compatible. According to Semantic Versioning (SemVer), this requires a minor version update. Please verify and update your version number accordingly.

---
+++ tangler/src/messages/finalized_commit.rs
@@ -0,0 +1,18 @@
+use std::path::PathBuf;
+use akton::prelude::*;
+use derive_more::*;
+use derive_new::new;
+use crate::models::{CommitMessage, CommittedCommit, Oid, TimeStamp};
+
+/// Represents a successful commit message with its details.
+#[derive(new, Default, Debug, Clone, )]
+pub(crate) struct FinalizedCommit {
+    pub(crate) when: TimeStamp,
+    pub(crate) target_file: PathBuf,
+    pub(crate) repository_nickname: String,
+    pub(crate) hash: String,
+    pub(crate) commit_message: CommitMessage
+}
+
+
+
