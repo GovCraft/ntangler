74abdd584b546e9525de9c0f21ecf755ec6c8d8d
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 02:25:42 -0600

    style(ui): reformat scope.rs file
    Reformats the scope.rs file for better readability and consistency. This change involves reordering and organizing the use statements and struct definitions to improve code structure.

--- tangler/src/models/ui/terminal/scope.rs
+++ tangler/src/models/ui/terminal/scope.rs
@@ -1,15 +1,18 @@
+use console::style;
+use owo_colors::colors::*;
+use owo_colors::{OwoColorize, Style};
 use std::default::Default;
 use std::fmt;
 use std::io::Write;
 use std::ops::Deref;
-use console::style;
-use owo_colors::colors::*;
-use owo_colors::{OwoColorize, Style};
 
+use crate::models::{
+    ConsoleStyle, DimStatic, OptionalScope, Scope, AMBER_12, GRAY_10, GRAY_11, GRAY_12, GRAY_3,
+    GRAY_9, SCOPE_COLOR, TEAL_11, TEAL_12, TEAL_9, WHITE_PURE,
+};
 use serde::Deserialize;
 use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 use tracing::{info, instrument};
-use crate::models::{TEAL_9, TEAL_12,SCOPE_COLOR, ConsoleStyle, OptionalScope, Scope, TEAL_11, AMBER_12, DimStatic, GRAY_12, GRAY_9, GRAY_11, WHITE_PURE, GRAY_3, GRAY_10};
 
 #[derive(Debug, Default, Clone, Deserialize)]
 pub(crate) struct ScopeTerminal(Scope);
@@ -27,7 +30,6 @@ impl Deref for ScopeTerminal {
 impl fmt::Display for ScopeTerminal {
     #[instrument(level = "trace", skip(self, f))]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-
         write!(f, "{}", self.0.style(*SCOPE_COLOR));
 
         Ok(())
