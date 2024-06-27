4224ed8ac1be29d7d30a7a1bab6dd1bf473ab84d
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 02:24:59 -0600

    style(models): fix formatting issues
    Correct formatting issues in tangled_repository.rs. Ensure consistent indentation and spacing for better readability and maintainability.

--- tangler/src/models/tangled_repository.rs
+++ tangler/src/models/tangled_repository.rs
@@ -1,10 +1,10 @@
-use std::path::{Path, PathBuf};
-use akton::prelude::Arn;
-use git2::Signature;
 use crate::models::config::RepositoryConfig;
 use crate::models::Oid;
+use akton::prelude::Arn;
 use derive_more;
+use git2::Signature;
 use serde::Deserialize;
+use std::path::{Path, PathBuf};
 
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub(crate) struct TangledRepository {
@@ -14,9 +14,9 @@ pub(crate) struct TangledRepository {
     pub(crate) branch_name: String,
 }
 
-impl From<RepositoryConfig> for TangledRepository{
+impl From<RepositoryConfig> for TangledRepository {
     fn from(value: RepositoryConfig) -> Self {
-        TangledRepository{
+        TangledRepository {
             akton_arn: Arn::with_root("tangled_repository").unwrap(),
             nickname: value.nickname,
             path: value.path,
