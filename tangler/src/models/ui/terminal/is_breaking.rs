ae513706181ee9f87b828aed91d98edefd348921
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 05:11:58 -0600

    fix(ui): remove duplicate imports
    This commit removes duplicate imports in the is_breaking.rs file. The duplicate imports of ConsoleStyle, ALERT_COLOR, BG_DARK, GRAY_10, GRAY_11, GRAY_12, GRAY_9, PUNCTUATION_COLOR, SCOPE_PUNCTUATION_COLOR, TEAL_11, TEAL_12, TIME_PUNCTUATION_COLOR, and WHITE_PURE are consolidated.
    
    BUG FIX: You appear to have made one or more backward-compatible bug fixes. According to Semantic Versioning (SemVer), this requires a patch version update. Please verify and update your version number accordingly.

--- tangler/src/models/ui/terminal/is_breaking.rs
+++ tangler/src/models/ui/terminal/is_breaking.rs
@@ -8,9 +8,8 @@ use termcolor::{Color, ColorSpec, StandardStream, WriteColor};
 use tracing::{info, instrument, trace};
 
 use crate::models::{
-    ConsoleStyle,  ALERT_COLOR, BG_DARK, GRAY_10, GRAY_11, GRAY_12, GRAY_9,
-    PUNCTUATION_COLOR, SCOPE_PUNCTUATION_COLOR, TEAL_11, TEAL_12, TIME_PUNCTUATION_COLOR,
-    WHITE_PURE,
+    ConsoleStyle, ALERT_COLOR, BG_DARK, GRAY_10, GRAY_11, GRAY_12, GRAY_9, PUNCTUATION_COLOR,
+    SCOPE_PUNCTUATION_COLOR, TEAL_11, TEAL_12, TIME_PUNCTUATION_COLOR, WHITE_PURE,
 };
 
 #[derive(Debug, Default, Clone, Deserialize, PartialEq)]
