5e77b8beac3bd81b52fca6eacb5d649226fd5705
Roland Rodriguez <roland@govcraft.ai>
2024-06-27 02:25:48 -0600

    style(messages): remove duplicate imports
    Remove duplicate imports of CommitMessage, CommittedCommit, and TimeStamp from the commit_finalized.rs file. This change cleans up the code by eliminating redundancy and improving readability.

--- tangler/src/messages/commit_finalized.rs
+++ tangler/src/messages/commit_finalized.rs
@@ -1,17 +1,14 @@
-use std::path::PathBuf;
+use crate::models::{CommitMessage, CommittedCommit, TimeStamp};
 use akton::prelude::*;
 use derive_more::*;
 use derive_new::new;
 use git2::Oid;
-use crate::models::{CommitMessage, CommittedCommit, TimeStamp};
+use std::path::PathBuf;
 
 /// Represents a successful commit message with its details.
-#[derive(new, Debug, Clone, )]
+#[derive(new, Debug, Clone)]
 pub(crate) struct CommitFinalized {
     pub(crate) path: PathBuf,
     pub(crate) commit_message: CommitMessage,
     pub(crate) oid: Oid,
 }
-
-
-
